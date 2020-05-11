b630648433d688df9496bd7d09a4bba78909b0e6
Lucas Easley <lucaseasley11@gmail.com>
2020-05-11 16:05:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,13 +1,13 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 1 , 'male', 'black', 'happy', 1) 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 13 , 'female', 'black', 'sad', 0) 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 51 , 'male', 'brown', 'happy', 1) 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 21 , 'male', 'black', 'sad', 0) 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 16 , 'female', 'brown', 'happy', 1) 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 61 , 'male', 'black', 'sad', 0) 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 13 , 'female', 'black', 'happy', 1) 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 100 , 'male', 'black', 'sad', 0) 
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 1 , 'male', 'black', 'happy', 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 13 , 'female', 'black', 'sad', 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 51 , 'male', 'brown', 'happy', 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 21 , 'male', 'black', 'sad', 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 16 , 'female', 'brown', 'happy', 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 61 , 'male', 'black', 'sad', 0)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 13 , 'female', 'black', 'happy', 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 100 , 'male', 'black', 'sad', 0)
 
-  
+
 )
 
 
